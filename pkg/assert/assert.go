a81cc181ca75279708ab6ebf132959f0cf8c40ce
Andrew Dunstall <andydunstall@hotmail.co.uk>
2025-06-01 06:03:59 +0100

    pkg/assert: add assert.Panicf

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -1,19 +1,40 @@
 package assert
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
+
+func Panicf(format string, a ...any) {
+	// Include the location as it could be buried in the middle
+	// of the panicking stack.
+	prefix := ""
+	if _, file, line, ok := runtime.Caller(1); ok {
+		prefix = fmt.Sprintf("%s:%d: ", file, line)
+	}
+	panic(prefix + fmt.Sprintf(format, a...))
+}
 
 func Assert(pred bool, msg string) {
-	if !pred {
-		panic("assert: " + msg)
-	}
+	assertf(pred, msg)
 }
 
 func Assertf(pred bool, format string, a ...any) {
-	if !pred {
-		panic("assert: " + fmt.Sprintf(format, a...))
-	}
+	assertf(pred, format, a...)
 }
 
-func Panicf(format string, a ...any) {
-	panic(fmt.Sprintf(format, a...))
+func assertf(pred bool, format string, a ...any) {
+	if !pred {
+		// Include the assertion location as it could be buried in the middle
+		// of the panicking stack.
+		//
+		// Use a depth of 2, since we want the caller or Assert/Assertf, not
+		// the caller of assertf.
+		prefix := ""
+		if _, file, line, ok := runtime.Caller(2); ok {
+			prefix = fmt.Sprintf("%s:%d: ", file, line)
+		}
+
+		panic("assert: " + fmt.Sprintf(prefix+format, a...))
+	}
 }
